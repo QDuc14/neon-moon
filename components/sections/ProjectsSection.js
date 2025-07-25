5f1c67474f3f52ebc2e52799f895636de93621e2
Duc Quach <ddquach14@gmail.com>
2025-07-25 17:02:32 +0700

    change hover to blue

--- components/sections/ProjectsSection.js
+++ components/sections/ProjectsSection.js
@@ -10,7 +10,7 @@ export default function ProjectsSection() {
           <Link
             href={project.link}
             key={idx}
-            className="block bg-purple-950/40 p-6 rounded-xl border border-purple-800 shadow-xl hover:shadow-purple-600 transition-shadow"
+            className="block bg-purple-950/40 p-6 rounded-xl border border-purple-800 shadow-xl hover:shadow-blue-600 transition-shadow"
           >
             <h3 className="text-2xl font-semibold text-light mb-2">{project.title}</h3>
             <p className="text-glow text-sm">{project.description}</p>
